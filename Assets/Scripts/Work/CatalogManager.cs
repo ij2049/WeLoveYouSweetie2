3a7032c53d01df92af53fa2730409e9c8ccecdd8
Ina Joe <ij2049@nyu.edu>
2022-12-30 19:25:32 -0500

    Working System4
    Catalog Manger/Controller

--- Assets/Scripts/Work/CatalogManager.cs
+++ Assets/Scripts/Work/CatalogManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public enum CatalogCardType
 {
@@ -12,12 +13,14 @@ public enum CatalogCardType
     Triangle_Green,
     Triangle_Yellow,
     Square_Yellow,
-    Square_Green,
+    Square_Purple,
     Infinite_Purple,
     Infinite_Yellow,
     Moon_Purple,
     Moon_Yellow,
-    Cloud,
+    Cloud_Green,
+    Cloud_purple,
+    Circle_Green,
 }
 
 [System.Serializable]
@@ -33,23 +36,63 @@ public class CatalogCardInfo
 
 public class CatalogManager : MonoBehaviour
 {
+    //cards info
     [SerializeField] 
     private CatalogCardInfo[] cardsInfo;
 
+    //randomize cards that is choice from cards infos
+    public CatalogCardInfo[] randomnizeChoicesCards;
+
+    //each of the cards in the unity
     [SerializeField] 
     private CatalogCardController[] cardController;
+    
+    //reset cardInfo
+    private CatalogCardInfo[] resetCardInfo;
+
+    private CatalogCardInfo temp_cardInfo;
+    private int[] tempNum = new int[8];
+
+    private void Awake()
+    {
+        resetCardInfo = cardsInfo;
+    }
+
     private void Start()
     {
         //put card info to the each card
-        
+        RandomCardsChoice();
         //instantiate cards img on the list
 
-        for (int i = 0; i < cardsInfo.Length; i++)
+        for (int i = 0; i < randomnizeChoicesCards.Length; i++)
         {
+            cardController[i].cardType = randomnizeChoicesCards[i].cardType;
             cardInfoInterpret(i, "head");
             cardInfoInterpret(i, "body");
             cardInfoInterpret(i, "leg");
         }
+    }
+
+    private void RandomCardsChoice()
+    {
+        for (int i = 0; i < cardsInfo.Length; i++)
+        {
+            int rnd = Random.Range(0, cardsInfo.Length);
+            
+            temp_cardInfo = cardsInfo[rnd];
+            cardsInfo[rnd] = cardsInfo[i];
+            cardsInfo[i] = temp_cardInfo;
+        }
+
+        for (int i = 0; i < 8; i++)
+        {
+            randomnizeChoicesCards[i] = cardsInfo[i];
+        }
+    }
+
+    private void ResetCardInfo()
+    {
+        cardsInfo = resetCardInfo;
     }
     
     //change card info enum info to the string
@@ -57,69 +100,79 @@ public class CatalogManager : MonoBehaviour
     {
         if (_partName == "head")
         {
-            if (cardsInfo[_i].head == CatalogCardType.Star_Yellow)
+            if (randomnizeChoicesCards[_i].head == CatalogCardType.Star_Yellow)
             {
                 cardController[_i].headType = "Star_Yellow";
             }
         
-            else if (cardsInfo[_i].head == CatalogCardType.Star_Purple)
+            else if (randomnizeChoicesCards[_i].head == CatalogCardType.Star_Purple)
             {
                 cardController[_i].headType = "Star_Purple";
             }
         
-            else if (cardsInfo[_i].head == CatalogCardType.OppTriangle_Green)
+            else if (randomnizeChoicesCards[_i].head == CatalogCardType.OppTriangle_Green)
             {
                 cardController[_i].headType = "OppTriangle_Green";
             }
             
-            else if (cardsInfo[_i].head == CatalogCardType.OppTriangle_Purple)
+            else if (randomnizeChoicesCards[_i].head == CatalogCardType.OppTriangle_Purple)
             {
                 cardController[_i].headType = "OppTriangle_Purple";
             }
             
-            else if (cardsInfo[_i].head == CatalogCardType.Triangle_Green)
+            else if (randomnizeChoicesCards[_i].head == CatalogCardType.Triangle_Green)
             {
                 cardController[_i].headType = "Triangle_Green";
             }
             
-            else if (cardsInfo[_i].head == CatalogCardType.Triangle_Yellow)
+            else if (randomnizeChoicesCards[_i].head == CatalogCardType.Triangle_Yellow)
             {
                 cardController[_i].headType = "Triangle_Yellow";
             }
             
-            else if (cardsInfo[_i].head == CatalogCardType.Square_Yellow)
+            else if (randomnizeChoicesCards[_i].head == CatalogCardType.Square_Yellow)
             {
                 cardController[_i].headType = "Square_Yellow";
             }
             
-            else if (cardsInfo[_i].head == CatalogCardType.Square_Green)
+            else if (randomnizeChoicesCards[_i].head == CatalogCardType.Square_Purple)
             {
-                cardController[_i].headType = "Square_Green";
+                cardController[_i].headType = "Square_Purple";
             }
             
-            else if (cardsInfo[_i].head == CatalogCardType.Infinite_Purple)
+            else if (randomnizeChoicesCards[_i].head == CatalogCardType.Infinite_Purple)
             {
                 cardController[_i].headType = "Infinite_Purple";
             }
             
-            else if (cardsInfo[_i].head == CatalogCardType.Infinite_Yellow)
+            else if (randomnizeChoicesCards[_i].head == CatalogCardType.Infinite_Yellow)
             {
                 cardController[_i].headType = "Infinite_Yellow";
             }
             
-            else if (cardsInfo[_i].head == CatalogCardType.Moon_Purple)
+            else if (randomnizeChoicesCards[_i].head == CatalogCardType.Moon_Purple)
             {
                 cardController[_i].headType = "Moon_Purple";
             }
             
-            else if (cardsInfo[_i].head == CatalogCardType.Moon_Yellow)
+            else if (randomnizeChoicesCards[_i].head == CatalogCardType.Moon_Yellow)
             {
                 cardController[_i].headType = "Moon_Yellow";
             }
             
-            else if (cardsInfo[_i].head == CatalogCardType.Cloud)
+            else if (randomnizeChoicesCards[_i].head == CatalogCardType.Cloud_Green)
             {
-                cardController[_i].headType = "Cloud";
+                cardController[_i].headType = "Cloud_Green";
+            }
+            
+            else if (randomnizeChoicesCards[_i].head == CatalogCardType.Cloud_purple)
+            {
+                cardController[_i].headType = "Cloud_purple";
+            }
+            
+            else if (randomnizeChoicesCards[_i].head == CatalogCardType.Circle_Green)
+            {
+                cardController[_i].headType = "Circle_Green";
             }
 
             else
@@ -130,71 +183,81 @@ public class CatalogManager : MonoBehaviour
         
         else if (_partName == "body")
         {
-            if (cardsInfo[_i].body == CatalogCardType.Star_Yellow)
+            if (randomnizeChoicesCards[_i].body == CatalogCardType.Star_Yellow)
             {
                 cardController[_i].bodyType = "Star_Yellow";
             }
         
-            else if (cardsInfo[_i].body == CatalogCardType.Star_Purple)
+            else if (randomnizeChoicesCards[_i].body == CatalogCardType.Star_Purple)
             {
                 cardController[_i].bodyType = "Star_Purple";
             }
         
-            else if (cardsInfo[_i].body == CatalogCardType.OppTriangle_Green)
+            else if (randomnizeChoicesCards[_i].body == CatalogCardType.OppTriangle_Green)
             {
                 cardController[_i].bodyType = "OppTriangle_Green";
             }
             
-            else if (cardsInfo[_i].body == CatalogCardType.OppTriangle_Purple)
+            else if (randomnizeChoicesCards[_i].body == CatalogCardType.OppTriangle_Purple)
             {
                 cardController[_i].bodyType = "OppTriangle_Purple";
             }
             
-            else if (cardsInfo[_i].body == CatalogCardType.Triangle_Green)
+            else if (randomnizeChoicesCards[_i].body == CatalogCardType.Triangle_Green)
             {
                 cardController[_i].bodyType = "Triangle_Green";
             }
             
-            else if (cardsInfo[_i].body == CatalogCardType.Triangle_Yellow)
+            else if (randomnizeChoicesCards[_i].body == CatalogCardType.Triangle_Yellow)
             {
                 cardController[_i].bodyType = "Triangle_Yellow";
             }
             
-            else if (cardsInfo[_i].body == CatalogCardType.Square_Yellow)
+            else if (randomnizeChoicesCards[_i].body == CatalogCardType.Square_Yellow)
             {
                 cardController[_i].bodyType = "Square_Yellow";
             }
             
-            else if (cardsInfo[_i].body == CatalogCardType.Square_Green)
+            else if (randomnizeChoicesCards[_i].body == CatalogCardType.Square_Purple)
             {
-                cardController[_i].bodyType = "Square_Green";
+                cardController[_i].bodyType = "Square_Purple";
             }
             
-            else if (cardsInfo[_i].body == CatalogCardType.Infinite_Purple)
+            else if (randomnizeChoicesCards[_i].body == CatalogCardType.Infinite_Purple)
             {
                 cardController[_i].bodyType = "Infinite_Purple";
             }
             
-            else if (cardsInfo[_i].body == CatalogCardType.Infinite_Yellow)
+            else if (randomnizeChoicesCards[_i].body == CatalogCardType.Infinite_Yellow)
             {
                 cardController[_i].bodyType = "Infinite_Yellow";
             }
             
-            else if (cardsInfo[_i].body == CatalogCardType.Moon_Purple)
+            else if (randomnizeChoicesCards[_i].body == CatalogCardType.Moon_Purple)
             {
                 cardController[_i].bodyType = "Moon_Purple";
             }
             
-            else if (cardsInfo[_i].body == CatalogCardType.Moon_Yellow)
+            else if (randomnizeChoicesCards[_i].body == CatalogCardType.Moon_Yellow)
             {
                 cardController[_i].bodyType = "Moon_Yellow";
             }
             
-            else if (cardsInfo[_i].body == CatalogCardType.Cloud)
+            else if (randomnizeChoicesCards[_i].body == CatalogCardType.Cloud_Green)
             {
-                cardController[_i].bodyType = "Cloud";
+                cardController[_i].bodyType = "Cloud_Green";
+            }
+            
+            else if (randomnizeChoicesCards[_i].body == CatalogCardType.Cloud_purple)
+            {
+                cardController[_i].bodyType = "Cloud_purple";
             }
 
+            else if (randomnizeChoicesCards[_i].body == CatalogCardType.Circle_Green)
+            {
+                cardController[_i].bodyType = "Circle_Green";
+            }
+            
             else
             {
                 Debug.Log("there is no right selection");
@@ -203,69 +266,79 @@ public class CatalogManager : MonoBehaviour
         
         else if (_partName == "leg")
         {
-            if (cardsInfo[_i].leg == CatalogCardType.Star_Yellow)
+            if (randomnizeChoicesCards[_i].leg == CatalogCardType.Star_Yellow)
             {
                 cardController[_i].legType = "Star_Yellow";
             }
         
-            else if (cardsInfo[_i].leg == CatalogCardType.Star_Purple)
+            else if (randomnizeChoicesCards[_i].leg == CatalogCardType.Star_Purple)
             {
                 cardController[_i].legType = "Star_Purple";
             }
         
-            else if (cardsInfo[_i].leg == CatalogCardType.OppTriangle_Green)
+            else if (randomnizeChoicesCards[_i].leg == CatalogCardType.OppTriangle_Green)
             {
                 cardController[_i].legType = "OppTriangle_Green";
             }
             
-            else if (cardsInfo[_i].leg == CatalogCardType.OppTriangle_Purple)
+            else if (randomnizeChoicesCards[_i].leg == CatalogCardType.OppTriangle_Purple)
             {
                 cardController[_i].legType = "OppTriangle_Purple";
             }
             
-            else if (cardsInfo[_i].leg == CatalogCardType.Triangle_Green)
+            else if (randomnizeChoicesCards[_i].leg == CatalogCardType.Triangle_Green)
             {
                 cardController[_i].legType = "Triangle_Green";
             }
             
-            else if (cardsInfo[_i].leg == CatalogCardType.Triangle_Yellow)
+            else if (randomnizeChoicesCards[_i].leg == CatalogCardType.Triangle_Yellow)
             {
                 cardController[_i].legType = "Triangle_Yellow";
             }
             
-            else if (cardsInfo[_i].leg == CatalogCardType.Square_Yellow)
+            else if (randomnizeChoicesCards[_i].leg == CatalogCardType.Square_Yellow)
             {
                 cardController[_i].legType = "Square_Yellow";
             }
             
-            else if (cardsInfo[_i].leg == CatalogCardType.Square_Green)
+            else if (randomnizeChoicesCards[_i].leg == CatalogCardType.Square_Purple)
             {
-                cardController[_i].legType = "Square_Green";
+                cardController[_i].legType = "Square_Purple";
             }
             
-            else if (cardsInfo[_i].leg == CatalogCardType.Infinite_Purple)
+            else if (randomnizeChoicesCards[_i].leg == CatalogCardType.Infinite_Purple)
             {
                 cardController[_i].legType = "Infinite_Purple";
             }
             
-            else if (cardsInfo[_i].leg == CatalogCardType.Infinite_Yellow)
+            else if (randomnizeChoicesCards[_i].leg == CatalogCardType.Infinite_Yellow)
             {
                 cardController[_i].legType = "Infinite_Yellow";
             }
             
-            else if (cardsInfo[_i].leg == CatalogCardType.Moon_Purple)
+            else if (randomnizeChoicesCards[_i].leg == CatalogCardType.Moon_Purple)
             {
                 cardController[_i].legType = "Moon_Purple";
             }
             
-            else if (cardsInfo[_i].leg == CatalogCardType.Moon_Yellow)
+            else if (randomnizeChoicesCards[_i].leg == CatalogCardType.Moon_Yellow)
             {
                 cardController[_i].legType = "Moon_Yellow";
             }
             
-            else if (cardsInfo[_i].leg == CatalogCardType.Cloud)
+            else if (randomnizeChoicesCards[_i].leg == CatalogCardType.Cloud_Green)
             {
-                cardController[_i].legType = "Cloud";
+                cardController[_i].legType = "Cloud_Green";
+            }
+            
+            else if (randomnizeChoicesCards[_i].leg == CatalogCardType.Cloud_purple)
+            {
+                cardController[_i].legType = "Cloud_purple";
+            }
+            
+            else if (randomnizeChoicesCards[_i].leg == CatalogCardType.Circle_Green)
+            {
+                cardController[_i].legType = "Circle_Green";
             }
 
             else
@@ -279,7 +352,7 @@ public class CatalogManager : MonoBehaviour
             Debug.LogError("card part type is not right");
         }
     }
-    
+
 }
 
 
